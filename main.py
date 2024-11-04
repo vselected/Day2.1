4b5d43bdb2c9fc6814d1deb2eb7ebcb344c841d5
vselected <dominik.sarleja@gmail.com>
2024-11-04 21:09:19 +0100

    Exercise I/O with Basic files in Python

--- main.py
+++ main.py
@@ -34,13 +34,40 @@ print(1==1)
 print(1>2)
 
 # I/O with Basic files in Python
-myfile = open("test.txt")
-print(myfile.read())
-myfile.seek(0)
-print(myfile.read())
-myfile.seek(0)
-print(myfile.readlines())
-myfile.close()
-with open("test.txt") as my_new_file:
-    contents = my_new_file.read()
-    print(contents)
+#myfile = open("test.txt")
+#print(myfile.read())
+#myfile.seek(0)
+#print(myfile.read())
+#myfile.seek(0)
+#print(myfile.readlines())
+#myfile.close()
+
+with open("test.txt", mode="r") as f:
+    print(f.read())
+
+with open("test.txt", mode="a") as f:
+    print(f.write("\nNew Line"))
+
+with open("test.txt", mode="r") as f:
+    print(f.read())
+
+with open("dadjidwada.txt", mode="w") as f:
+    print(f.write("New file"))
+
+with open("dadjidwada.txt", mode="r") as f:
+    print(f.read())
+
+# Exercise
+# File I/O
+# This exercise will require several lines of code.
+# Write a script that opens a file named 'test.txt' , writes 'Hello World'  to the file, then closes it.
+# For example, the following code opens a file called 'myfile.txt' , writes 'This is my file' , and closes it:
+# x = open('myfile.txt', 'w')
+# x.write('This is my file')
+# x.close()
+
+with open("test.txt", mode="w") as f:
+    f.write("Hello World")
+    f.close()
+
+
